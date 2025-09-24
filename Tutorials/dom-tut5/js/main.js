8394c638da0ee5c81a7a3f1833c3f8efa52ee1c7
woojinnies <laurenashleyanderson0220@gmail.com>
2025-09-23 21:10:00 -0700

    Tut5

---
+++ Tutorials/dom-tut5/js/main.js
@@ -0,0 +1,18 @@
+
+document.querySelector('#lady').addEventListener('click', function(){
+    document.querySelector('#lady').style.display = 'none'
+    alert("Time to Go Outside!")
+})
+document.querySelector('#heart-girl').addEventListener('click', function(){
+    document.querySelector('#heart-girl').style.display = 'none'
+    alert("Please Don't Break my Heart....")
+})
+document.querySelector('#field-girl').addEventListener('click', function(){
+    document.querySelector('#field-girl').style.display = 'none'
+    alert("I Love Nature!")
+})
+document.querySelector('#tree-boy').addEventListener('click', function(){
+    document.querySelector('#tree-boy').style.display = 'none'
+
+    alert("Would you Like an Apple?")
+})
