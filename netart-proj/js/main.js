94f3e531d35cf56ae1f4ad568fcb3bbd3d1b9356
woojinnies <laurenashleyanderson0220@gmail.com>
2025-10-05 21:20:52 -0700

    trying to get this to work

--- netart-proj/js/main.js
+++ netart-proj/js/main.js
@@ -1,15 +1,18 @@
-document.querySelector('#dark-girl').addEventListener('click', function(){
-    document.querySelector('#girl-in-dark').style.display = 'visible'
-    alert("He Talks to me all the time but ignored me today... He's only supposed to talk to me...")
-})
-document.querySelector('#girl-in-dark').addEventListener('click', function(){
-    document.querySelector('#obessive').style.display = 'visible'
-    alert(" He's Mine and Only Mine...")
-})
-document.querySelector('#obessive').addEventListener('click', function(){
-    document.querySelector('#parasocial').style.display = 'visible'
-    alert("We're Meant for Each Other!")
-})
-document.querySelector('#parasocial').addEventListener('click', function(){
-    alert("Why is she Talking to Him?!")
-})
+document.querySelector('#dark-girl').addEventListener('click', function() {
+    document.querySelector('#in-dark').classList.remove('hidden');
+    alert("He Talks to me all the time but ignored me today... He's only supposed to talk to me...");
+});
+
+document.querySelector('#in-dark').addEventListener('click', function() {
+    document.querySelector('#obessive').classList.remove('hidden');
+    alert("He's Mine and Only Mine...");
+});
+
+document.querySelector('#obessive').addEventListener('click', function() {
+    document.querySelector('#parasocial').classList.remove('hidden');
+    alert("We're Meant for Each Other!");
+});
+
+document.querySelector('#parasocial').addEventListener('click', function() {
+    alert("Why is she Talking to Him?!");
+});
