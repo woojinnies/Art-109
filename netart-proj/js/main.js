ef325bb546ce5b76d5e087b5c3015fa4d0bbc590
woojinnies <laurenashleyanderson0220@gmail.com>
2025-10-05 20:35:13 -0700

    addition of js

---
+++ netart-proj/js/main.js
@@ -0,0 +1,15 @@
+document.querySelector('#dark-girl').addEventListener('click', function(){
+    document.querySelector('#in-dark').style.display = 'visible'
+    alert("He Talks to me all the time but ignored me today... He's only supposed to talk to me...")
+})
+document.querySelector('#in-dark').addEventListener('click', function(){
+    document.querySelector('#obessive').style.display = 'visible'
+    alert(" He's Mine and Only Mine...")
+})
+document.querySelector('#obessive').addEventListener('click', function(){
+    document.querySelector('#parasocial').style.display = 'visible'
+    alert("We're Meant for Each Other!")
+})
+document.querySelector('#parasocial').addEventListener('click', function(){
+    alert("Why is she Talking to Him?!")
+})
