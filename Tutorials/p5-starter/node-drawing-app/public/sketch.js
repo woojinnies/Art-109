8dc57db9428e631a0b6d9d3ab67ddfd4055e960c
woojinnies <laurenashleyanderson0220@gmail.com>
2025-11-11 14:07:15 -0800

    tut 10

---
+++ Tutorials/p5-starter/node-drawing-app/public/sketch.js
@@ -0,0 +1,36 @@
+
+var socket;
+
+function setup() {
+ createCanvas(500,500);
+  background(51);
+
+    socket = io.connect('http://localhost:3000');
+    socket.on('mouse', newDrawing);
+}
+
+
+function newDrawing(data){
+    noStroke();
+    fill(255, 0, 100);
+    ellipse(data.x, data.y, 60, 60);
+}
+
+function mouseDragged(){
+    console.log(mouseX + ',' + mouseY);
+
+    var data = {
+        x: mouseX,
+        y: mouseY
+    }
+
+    socket.emit('mouse', data);
+
+    noStroke();
+    fill(255);
+    ellipse(mouseX, mouseY, 60, 60);
+}
+
+function draw() {
+
+}
